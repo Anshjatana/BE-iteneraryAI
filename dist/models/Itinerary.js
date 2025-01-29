e0c9f5e9ff969686a22f113503377200a32de671
Ansh Jatana <anshjatana1111@gmail.com>
2025-01-29 16:42:31 +0530

    Fix: ts config

--- dist/models/Itinerary.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
-// Create a model without defining a schema
-const Itinerary = mongoose_1.default.model('Itinerary', new mongoose_1.default.Schema({}, { strict: false }));
-exports.default = Itinerary;
